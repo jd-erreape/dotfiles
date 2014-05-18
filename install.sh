fdaa580a1fe89ce3b7cc28274fdf70d636e2eabc
Juan de Dios Herrero <juandediosherrero@gmail.com>
2014-05-18 12:53:50 +0200

    Add credits to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#
+# Credits to:  http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+#
+#############################
 
 ########## Variables
 
